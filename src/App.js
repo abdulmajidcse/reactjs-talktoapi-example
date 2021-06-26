895906546929c7287be97b586bc068dfc544d98e
Abdul Majid <abdulmajidcse@gmail.com>
2021-06-26 23:33:34 +0600

    Navbar component and install react router dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="container">
-      Welcome to React App.
+      <Navbar />
+      <div className="card">
+        <div className="card-header">
+          <h4>Welcome to React App.</h4>
+        </div>
+        <div className="card-body">
+          Home page.
+        </div>
+      </div>
     </div>
   );
 }
