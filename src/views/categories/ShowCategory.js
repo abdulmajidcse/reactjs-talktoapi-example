8f33729e977f33d5da430e6e366dd03c931b2399
Abdul Majid <abdulmajidcse@gmail.com>
2021-07-01 16:40:34 +0600

    category show complete

--- src/views/categories/ShowCategory.js
+++ src/views/categories/ShowCategory.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router-dom';
+import Api from '../../config/Api';
+import Spinner from '../../components/Spinner';
+import Swal from 'sweetalert2';
 
 class ShowCategory extends React.Component {
-    render() {
+
+    state = {
+        category: {},
+        loading: true,
+    }
+
+    componentDidMount() {
         let { match } = this.props;
+
+        Api.get(`/categories/${match.params.id}`)
+        .then(response => {
+            this.setState({
+                category: response.data.category,
+                loading: false,
+            });
+        })
+        .catch(() => {
+            this.setState({
+                loading: false
+            });
+            Swal.fire('', 'There was a problem fetching!', 'error');
+        });
+    }
+
+    render() {
+        let { category, loading } = this.state;
         return (
             <div className="card">
+                <Spinner loading={loading} />
                 <div className="card-header">
                     <div className="row">
                     <div className="col-md-6">
@@ -18,7 +46,9 @@ class ShowCategory extends React.Component {
                     </div>
                 </div>
                 <div className="card-body">
-                    category ID: { match.params.id }
+                    <p>ID: { category.id }</p>
+                    <p>Name: { category.name }</p>
+                    <p>Created At: { category.created_at }</p>
                 </div>
             </div>
         );
