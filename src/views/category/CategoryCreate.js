3d25b54e313ff0db316c9288315712d67253f3fb
Abdul Majid <abdulmajidcse@gmail.com>
2021-07-06 10:16:02 +0600

    useReducer hook

--- src/views/category/CategoryCreate.js
+++ src/views/category/CategoryCreate.js
@@ -1,61 +1,86 @@
 import { Link, useHistory } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useReducer, useEffect } from "react";
 import Spinner from "../../components/Spinner";
 import Api from '../../config/Api';
 import Swal from "sweetalert2";
 
 export default function CategoryCreate() {
-    let [loading, setLoading] = useState(true);
-    let [name, setName] = useState('');
-    let [error, setError] = useState('');
     const history = useHistory();
+    // let [loading, setLoading] = useState(true);
+    // let [name, setName] = useState('');
+    // let [error, setError] = useState('');
+
+    // useReducer mechanism
+    const initialSate = {
+        name: '',
+        loading: true,
+        error: '',
+    };
+
+    const init = (initialSate) => {
+        return initialSate;
+    };
+
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case 'name':
+                return {name: action.value, loading: false, error: ''};
+            case 'loading':
+                return {...state, loading: action.value};
+            case 'error':
+                return {...state, error: action.value};
+            case 'loadingError':
+                return {...state, loading: action.loading, error: action.error};
+            case 'reset':
+                return init(initialSate);
+            default:
+                return state;
+        }
+    };
+
+    const [state, dispatch] = useReducer(reducer, initialSate, init);
 
     useEffect(() => {
         document.title = 'New Category - React App';
-        setLoading(false);
+        dispatch({type: 'loading', value: false});
 
         return () => {
-            setName('');
-            setLoading(false);
-            setError('');
+            dispatch({type: 'reset'});
         };
     }, []);
 
     let handleChange = e => {
-        setError('');
-        setName(e.target.value);
+        dispatch({type: 'name', value: e.target.value});
     };
 
     let handleForm = e => {
         e.preventDefault();
 
-        if (loading) return true;
-
-        setLoading(true);
-        setError('');
+        if (state.loading) return true;
+        dispatch({type: 'loadingError', loading: true, error: ''});
 
         let data = new FormData();
-        data.append('name', name);
+        data.append('name', state.name);
 
         Api.post('/categories', data)
         .then((response) => {
             Swal.fire('', response.data.success, 'success');
-            setLoading(false);
+            dispatch({type: 'loading', value: false});
             history.push('/categories');
         })
         .catch(error => {
             if (error.response) {
-                setError(error.response.data.name[0]);
+                dispatch({type: 'error', value: error.response.data.name[0]});
             } else {
                 Swal.fire('', 'Something went wrong!', 'error');
             }
-            setLoading(false);
+            dispatch({type: 'loading', value: false});
         });
     };
 
     return (
         <div className="card">
-            <Spinner loading={loading} />
+            <Spinner loading={state.loading} />
             <div className="card-header">
                 <div className="d-flex justify-content-between">
                     <h4>New Category</h4>
@@ -65,8 +90,8 @@ export default function CategoryCreate() {
             <div className="card-body">
                 <form onSubmit={handleForm}>
                     <label htmlFor="name" className="mb-2">Name</label>
-                    <input type="text" id="name" name="name" className={(error && 'is-invalid') + ' form-control'} value={name} onChange={handleChange} />
-                    {error && <div className="text-danger">{error}</div>}
+                    <input type="text" id="name" name="name" className={(state.error && 'is-invalid') + ' form-control'} value={state.name} onChange={handleChange} />
+                    {state.error && <div className="text-danger">{state.error}</div>}
                     <button type="submit" className="btn btn-primary mt-2">Save</button>
                 </form>
             </div>
